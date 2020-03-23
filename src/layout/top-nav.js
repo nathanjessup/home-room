712c0562b30da2717ceece5e6898926195455b94
Nathan Jessup <nathanj.jessup@gmail.com>
2020-03-22 23:12:41 -0400

    High-Five

---
+++ src/layout/top-nav.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  Text,
+} from 'react-native';
+
+const TopNav: () => React$Node = () => {
+  return (
+    <>
+        <Text>Mama Mia! I'm a header!</Text>
+    </>
+  );
+};
+
+export default TopNav;
